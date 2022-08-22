baa85c212a36d3088abcdac2ef78cd344bfbfa0e
Djoudi Cherfi <djoudi.cherfi@gmail.com>
2022-08-22 19:43:06 +0200

    Tableau avec boucle for

--- array.c
+++ array.c
@@ -3,20 +3,24 @@
 int main() {
 
     // type nom[taille];
-    int tab[3];
+    int tab[10];
     // int tab[3] = {10, 20, 30};
+    int length = 10;
     int i;
+
+    // int tab[n];
+    // Indices : 0 à n-1
 
     // nom[cle/indice] = valeur;
     // tab[0] = 1;
     // tab[1] = 2;
     // tab[2] = 3;
 
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < length; i++) {
         tab[i] = i + 1;
     }
 
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < length; i++) {
         printf("Mon nombre tab[%d] vaut %d\n", i, tab[i]);
     }
 
